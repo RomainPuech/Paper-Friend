bad331d7fc8518cd8a73433e86b096c55fc747ec
Artem Mamichev <70771092+artem-mamichev@users.noreply.github.com>
2022-11-24 14:14:05 +0100

    Implemented SD function

--- Code/PaperFriend/Analysis/DataAnalysis.cpp
+++ Code/PaperFriend/Analysis/DataAnalysis.cpp
@@ -1,1 +1,26 @@
 #include "DataAnalysis.h"
+#include "math.h"
+#include <vector>
+
+using namespace std;
+
+double DataAnalysis::calculateSD(vector<double> data){
+    /**
+     * @param vector data.
+     * @return standart deviation.
+     */
+    double sum = 0.0, mean, standardDeviation = 0.0;
+
+    for(vector<double>::iterator i = data.begin(); i < data.end(); i++){
+        sum += *i;
+    }
+
+    mean = sum / data.size();
+
+    for(vector<double>::iterator i = data.begin(); i < data.end(); i++) {
+        standardDeviation += pow(*i - mean, 2);
+    }
+    standardDeviation /= data.size();
+
+    return standardDeviation;
+}
