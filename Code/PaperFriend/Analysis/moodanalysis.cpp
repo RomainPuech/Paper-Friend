d721ac339b17a474d6a0427155bb8fb85ab04a9c
Brook-B-Nigatu <burabalke7776@gmail.com>
2022-11-24 12:05:59 +0100

    alert_depression declaration and definition in MoodAnalysis class. get_lastn_average declaration in MoodAnalysis class
    need get_lastn_average to define alert_depression. get_lastn_average not yet defined

--- Code/PaperFriend/Analysis/moodanalysis.cpp
+++ Code/PaperFriend/Analysis/moodanalysis.cpp
@@ -1,3 +1,6 @@
 #include "moodanalysis.h"
 
 
+bool MoodAnalysis::alert_depression(int n, int m){
+    return get_lastn_average(n) < get_lastn_average(m);
+}
--- Code/PaperFriend/Analysis/moodanalysis.h
+++ Code/PaperFriend/Analysis/moodanalysis.h
@@ -3,7 +3,10 @@
 # include "DataAnalysis.h";
 class MoodAnalysis : public DataAnalysis{
 private:
+    double get_lastn_average(int n); // This will compute the average mood over the last n days.
+                                     // Not implemented yet.
 public:
+    bool alert_depression(int n, int m);
 };
 
 
