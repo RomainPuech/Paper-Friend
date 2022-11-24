caa43697428b417addb700230167e060ca463694
Elyndrium <91188350+Elyndrium@users.noreply.github.com>
2022-11-24 11:47:22 +0100

    Added "average_vector" function template.
    The function takes in a vector of any type of number and returns a double corresponding to the average.

--- Code/PaperFriend/Analysis/moodanalysis.h
+++ Code/PaperFriend/Analysis/moodanalysis.h
@@ -1,8 +1,19 @@
 #ifndef MOODANALYSIS_H
 #define MOODANALYSIS_H
 
+#include <vector>
 
 
+template <typename T> double average_vector(std::vector<T> vector){
+    T sum = 0;
+    for (auto i=vector.begin(); i<vector.end(); i++){
+        sum = sum + *i;
+    }
+    double sumD = sum;
+    double vecsizeD = vector.size();
+    double average = sumD/vecsizeD;
+    return average;
+}
 
 
 #endif // MOODANALYSIS_H
