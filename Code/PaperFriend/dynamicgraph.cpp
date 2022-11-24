c84e98880788521f8b45bd2315cb30985b57333e
Romain Puech <romain.puech@polytechnique.edu>
2022-11-24 09:48:49 +0100

    Added Qt Chart library

---
+++ Code/PaperFriend/dynamicgraph.cpp
@@ -0,0 +1,19 @@
+#include "dynamicgraph.h"
+#include <QApplication>
+#include <QtCharts>
+// Represents 1 set of bars in a bar chart
+#include <QtCharts/QBarSet>
+
+// Displays the color used to represent each
+// QBarSet
+#include <QtCharts/QLegend>
+// Adds categories to the charts axes
+#include <QtCharts/QBarCategoryAxis>
+// Used to create a line chart
+#include <QtCharts/QLineSeries>
+// Used to change names on axis
+#include <QtCharts/QCategoryAxis>
+
+DynamicGraph::DynamicGraph()
+{
+}
---
+++ Code/PaperFriend/dynamicgraph.h
@@ -0,0 +1,12 @@
+#ifndef DYNAMICGRAPH_H
+#define DYNAMICGRAPH_H
+
+#include <QWidget>
+
+class DynamicGraph
+{
+public:
+    DynamicGraph();
+};
+
+#endif // DYNAMICGRAPH_H
