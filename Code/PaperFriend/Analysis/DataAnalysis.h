9fef6864f03e4f5ecc20a0b9562e4f6595e3804f
Luigi Massacci <luigi.massacci.it@gmail.com>
2022-11-28 14:00:37 +0100

    Leveraging STL for data analysis

--- Code/PaperFriend/Analysis/DataAnalysis.h
+++ Code/PaperFriend/Analysis/DataAnalysis.h
@@ -1,6 +1,7 @@
 #ifndef DATAANALYSIS_H
 #define DATAANALYSIS_H
 
+#include <numeric>
 #include <vector>
 
 using namespace std;
@@ -10,9 +11,20 @@ class DataAnalysis {
 public:
   double stddev(vector<double> data); // calculates standart deviation
 
-  template <typename T> double mean(vector<T> data);
+  template <typename T> double avg(vector<T> data) {
+
+    // prefer STL when available
+    double sum = std::accumulate(data.begin(), data.end(), 0.0);
+
+    return sum / data.size();
+  }
 
 private:
 };
+
+/**
+ * @param vector data.
+ * @return mean.
+ */
 
 #endif // DATAANALYSIS_H
