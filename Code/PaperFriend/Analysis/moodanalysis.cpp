3cec603543774acc094f338d50a8224f8cb92f42
Artem Mamichev <70771092+artem-mamichev@users.noreply.github.com>
2022-12-01 17:04:59 +0100

    Suggestions work

--- Code/PaperFriend/Analysis/moodanalysis.cpp
+++ Code/PaperFriend/Analysis/moodanalysis.cpp
@@ -7,52 +7,6 @@
 #include <algorithm>
 #include <map>
 
-// MoodAnalysis implementation:
-//double MoodAnalysis::get_lastn_average(int n, std::string item){
-    /**
-     * @param int n: number of n last days we take into account, string item: name of item (e.g. "mood" or "screen_time")
-     * @return double average mood for last n days
-     */
- /*   std::vector<double> mood_list{};
-    std::vector<double> sleep_list{};
-    std::vector<double> eating_healthy_list{};
-    std::vector<double> productivity_list{};
-    std::vector<double> communications_list{};
-    std::vector<double> screen_time_list{};
-
-    for(int i = std::max<int>(0, log.size() - n); i < log.size(); i++){
-        if (item == "eating_healthy") {
-            eating_healthy_list.push_back((log.begin() + i)->get_eating_healthy());
-        } else if (item == "sleep") {
-            sleep_list.push_back((log.begin() + i)->get_sleep());
-        } else if (item == "productivity") {
-            productivity_list.push_back((log.begin() + i)->get_productivity());
-        } else if (item == "communications") {
-            communications_list.push_back((log.begin() + i)->get_communications());
-        } else if (item == "screen_time") {
-            screen_time_list.push_back((log.begin() + i)->get_screen_time());
-        } else {
-            mood_list.push_back((log.begin() + i)->get_mood());
-        }
-    }
-
-    if (item == "eating_healthy") {
-        return avg<double>(eating_healthy_list);
-    } else if (item == "sleep") {
-        return avg<double>(sleep_list);
-    } else if (item == "productivity") {
-        return avg<double>(productivity_list);
-    } else if (item == "communications") {
-        return avg<double>(communications_list);
-    } else if (item == "screen_time") {
-        return avg<double>(screen_time_list);
-    } else {
-        return avg<double>(mood_list);
-    }
-
-}*/
-
-
 
 bool MoodAnalysis::alert_depression(int n, int m){
     /**
@@ -129,21 +83,21 @@ std::vector<Variables> MoodAnalysis::item_priority(){
     return vec;
 }
 
-//std::string MoodAnalysis::suggestion(std::vector<EntryPerso> log){  // some more exciting gameplay can be implementen later
+std::string MoodAnalysis::suggestion(std::vector<EntryPerso> log){  // some more exciting gameplay can be implementen later
     /**
     * @param NAN
     *
     * @returns string "general mood review + suggestion"
     */
- /*   std::string str{};
-    if (log.end()->get_mood() >= get_lastn_average(7, "mood")){
+    std::string str{};
+    if (log.end()->get_mood() >= get_lastn_average(7, MOOD)){
         str += "Your mood today is better than averadge! \n";
-        str += "For now " + ((*(item_priority(log).begin())))+ " and " + *(item_priority(log).begin() + 1) + "improve your mood the most, keep it up!";  // suggest top two items which affected the mood the most
+        str += "Your progress in" + var_to_str(*(item_priority().begin())) + " and " + var_to_str(*(item_priority().begin() + 1)) + "improves your mood the most, keep it up!";  // suggest top two items which affected the mood the most
     }
     else{
         str += "Your mood today is less than averadge:( \n";
-        str += "Try to increase " + *(item_priority(log).begin()) + " and " + *(item_priority(log).begin() + 1) + "!";
+        str += "Try to wrok on your " + var_to_str(*(item_priority().begin())) + " and " + var_to_str(*(item_priority().begin() + 1)) + "!";
     }
 
     return str;
-}*/
+}
