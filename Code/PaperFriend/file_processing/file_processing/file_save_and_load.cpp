c1094c0935d45f3ac2a9cb20c78fcd69072d2c15
thomas-Turkieh <Thomasturkieh@gmail.com>
2022-12-09 19:38:24 +0100

    implementing save and load functions for entryperso

--- Code/PaperFriend/file_processing/file_processing/file_save_and_load.cpp
+++ Code/PaperFriend/file_processing/file_processing/file_save_and_load.cpp
@@ -79,3 +79,167 @@ Entry load_entry_decrypt(std::string filename, std::string key){
     res.set_date(decode_string(j["date"], key));
     return res;
 }
+
+
+
+
+std::string activity_to_string(Activity act){//function that return a string that resume the activities to be saved
+    std::string name = act.get_name();
+    std::string value = std::to_string(act.get_value());
+    std::string type = std::to_string(act.get_type());
+    std::string delimiter = "$*$";
+    std::string delimiter2 = "*$*";
+    return (name+delimiter + value + delimiter+type + delimiter2);
+};
+
+Activity string_to_activity(std::string act){//function that takes a 'string resume' of an activity and return an initialized and corresponding Activity object
+    std::string delimiter = "$*$";
+    std::string temp = act.substr(0, act.find(delimiter));//extract the name
+    std::string name = temp;
+    act.erase(0, act.find(delimiter) + delimiter.length());
+    temp = act.substr(0, act.find(delimiter));//extract the value
+    double value = std::stod(temp);
+    act.erase(0, act.find(delimiter) + delimiter.length());
+    temp = act.substr(0, act.find(delimiter));//extract the type
+    int type= std::stoi(temp);
+    Activity res = Activity(name, value); //initialize the result
+    res.set_type(type);
+    return res;
+};
+
+
+std::string activities_vec_to_str (std::vector<Activity*> acts){//takes a vector of pointers to activities and return a string resuming all the vector (to be saved)
+    std::string res;
+    for(int i=0; i < acts.size(); i++){
+       res += activity_to_string(*acts[i]);
+    }
+    return res;
+}
+
+std::vector<Activity*> str_to_vec_activities (std::string act){//activity vec from a string
+    std::vector <Activity*> res;
+    std::string delimiter = "*$*";
+
+    size_t pos = 0;
+    std::string temp;
+    while ((pos = act.find(delimiter)) != std::string::npos) {
+        temp = act.substr(0, pos);
+        Activity* activity = new Activity(string_to_activity(temp));
+        res.push_back(activity);
+        act.erase(0, pos + delimiter.length());
+    }
+    return res;
+
+}
+
+
+
+std::string friend_to_string(Friend act){//function that return a string that resume the friend to be saved
+    std::string name = act.get_name();
+    std::string duration = std::to_string(act.get_duration());
+    std::string delimiter = "$*$";
+    std::string delimiter2 = "*$*";
+    return (name+delimiter +duration + delimiter2);
+};
+
+Friend string_to_frend(std::string act){//function that takes a 'string resume' of a friend and return an initialized and corresponding friend object
+    std::string delimiter = "$*$";
+    std::string temp = act.substr(0, act.find(delimiter));//extract the name
+    std::string name = temp;
+    act.erase(0, act.find(delimiter) + delimiter.length());
+    temp = act.substr(0, act.find(delimiter));//extract the type
+    int duration = std::stoi(temp);
+    Friend res = Friend(name, duration); //initialize the result
+    return res;
+};
+
+
+std::string friend_vec_to_str (std::vector<Friend*> acts){//takes a vector of pointers to Friend and return a string resuming all the vector (to be saved)
+    std::string res;
+    for(int i=0; i < acts.size(); i++){
+       res += friend_to_string(*acts[i]);
+    }
+    return res;
+}
+
+std::vector<Friend*> str_to_vec_friends (std::string act){//activity vec from a string
+    std::vector <Friend*> res;
+    std::string delimiter = "*$*";
+
+    size_t pos = 0;
+    std::string temp;
+    while ((pos = act.find(delimiter)) != std::string::npos) {
+        temp = act.substr(0, pos);
+        Friend* activity = new Friend(string_to_frend(temp));
+        res.push_back(activity);
+        act.erase(0, pos + delimiter.length());
+    }
+    return res;
+
+}
+
+
+
+
+bool save_entryperso(EntryPerso entry){ // I create and save the entry file, title format MM.YY.JJ
+
+    std::string activities = activities_vec_to_str(entry.get_activities());
+    std::string friends = friend_vec_to_str(entry.get_friends());
+    std::cout<<activities<<"\n";
+    std::cout<<friends<<"\n";
+
+
+    nlohmann::json  j = {
+        {"text", entry.get_text()},
+        {"title", entry.get_title()},
+        {"date", entry.get_date()},
+
+        {"mood", entry.get_mood()},
+        {"sleep", entry.get_sleep()},
+        {"eating_healthy",entry.get_eating_healthy()},
+        {"productivity", entry.get_productivity()},
+        {"communications", entry.get_communications()},
+        {"screen_time", entry.get_screen_time()},
+        {"activities",activities},
+
+        {"friends", friends}
+    };
+    std::string filename = entry.get_qdate().toString("MM.dd.yyyy").toStdString()+".json";
+
+    std::ofstream o("./" + filename);
+
+
+    if(!o.is_open()){
+        std::cout << "Error opening file" << std::endl;
+        return false;
+    }
+    o << j << std::endl;
+    std::cout<<"ouii";
+    o.close();
+    std::cout<<"nonnn";
+    return true;
+};
+
+
+
+
+
+
+EntryPerso load_entryperso(std::string filename){//retrieve the data of a Json file and return an initialized Entry object with this data
+    std::ifstream i(filename);
+    nlohmann::json j;
+    i >> j;
+    EntryPerso res = EntryPerso(j["text"], j["title"], str_to_vec_activities(j["activities"]), str_to_vec_friends(j["friends"]), j["mood"], j["sleep"], j["eating_healthy"], j["productivity"],j["communications"], j["screen_time"] );
+    res.set_date(j["date"]);
+    return res;
+}
+
+
+
+
+
+
+
+
+
+
--- Code/PaperFriend/file_processing/file_processing/file_save_and_load.h
+++ Code/PaperFriend/file_processing/file_processing/file_save_and_load.h
@@ -6,6 +6,8 @@
 #include <iostream>
 #include <fstream>
 #include "entryclasses.h"
+#include "activityclasses.h"
+#include "friendclasses.h"
 
 bool save_entry(Entry entry);
 
@@ -15,5 +17,29 @@ bool save_entry_encrypt(Entry entry, std::string path, std::string key);
 Entry load_entry(std::string filename);
 
 Entry load_entry_decrypt(std::string filename, std::string key);
+
+std::string activity_to_string(Activity act);
+
+Activity string_to_activity(std::string act);
+
+std::string activities_vec_to_str (std::vector<Activity*> acts);
+
+
+std::vector<Activity*> str_to_vec_activities (std::string act);
+
+std::string friend_to_string(Friend act);
+
+
+Friend string_to_frend(std::string act);
+
+std::string friend_vec_to_str (std::vector<Friend*> acts);
+
+std::vector<Friend*> str_to_vec_friends (std::string act);
+
+
+bool save_entryperso(EntryPerso entry);
+
+
+EntryPerso load_entryperso(std::string filename);
 
 #endif
